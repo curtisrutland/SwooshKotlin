4d92051c93883bf05eccca68229e648ac85af0ec
Curtis Rutland <curtisrutland@gmail.com>
2017-11-10 17:11:07 -0600

    lesson 5 commit

--- app/src/main/java/com/curtisrutland/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/curtisrutland/swoosh/LeagueActivity.kt
@@ -1,11 +1,19 @@
 package com.curtisrutland.swoosh
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun nextClicked(view: View) {
+        Log.i("LEAGUE", "next clicked")
+        startActivity(Intent(this, SkillActivity::class.java))
     }
 }
